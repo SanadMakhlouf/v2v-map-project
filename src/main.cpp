39989c6f79c6c5f2a0bfd8c6679694ca216c50e8
didou6661 <makhlouf.sanad@gmail.com>
2025-12-14 01:59:37 +0100

    ..

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,8 @@ int main(int argc, char** argv) {
 
     // Utiliser un timer pour s'assurer que le widget est complètement rendu avant de définir le centre
     QTimer::singleShot(0, &view, [&view]() {
-        view.setCenterLatLon(47.75, 7.335888, 14);
-       // view.setCenterLatLon(48.8566, 2.3522, 5); // paris
+        view.setCenterLatLon(47.75, 7.335888, 14); // mulhouse
+        //view.setCenterLatLon(48.8566, 2.3522, 15); // paris ses cordonees
     });
 
     return app.exec();
